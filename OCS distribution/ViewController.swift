e7833d54d63975599f22ef129f424d124aa0712a
serge <pyrov.sergey@gmail.com>
2019-04-24 18:19:04 +0300

    Initial Commit

---
+++ OCS distribution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCS distribution
+//
+//  Created by Sergey on 24/04/2019.
+//  Copyright © 2019 PyrovSergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
